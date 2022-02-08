e25592497e02bf4d28f1bbc2ff562793005061b7
Kasturi Wagh <88839908+KasturiWagh@users.noreply.github.com>
2022-02-08 15:16:04 +0530

    Add files via upload

---
+++ infosys.py
@@ -0,0 +1,34 @@
+arr = []                     #All sequences  
+curr = []                    #current value
+
+def check(a):
+    curr = a
+    if (curr % 2 == 0):
+        curr = a / 2
+        arr.append(curr)
+    elif (curr % 2 != 0):
+        curr = ( a * 3) + 1
+        arr.append(curr)
+    elif (curr == 0):
+        arr.append(0)
+
+    while (arr[-1] != 1):
+        check(curr)
+
+val = []                      #Input values 
+val = input().split()
+
+last = []                     #Sequence count
+
+for i in val :
+    aval = int(i)
+    if aval == 1:                      
+        last.append(1)
+    else:
+        arr = []
+        arr.append(aval)
+        curr = aval
+        check(curr)
+        last.append(len(arr))
+
+print(*last,sep = ",")
